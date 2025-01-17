0cce1932378e0df611ef34caae713cd7a4502e3f
Vinothkumar <146345635+Vinothkumar2711@users.noreply.github.com>
2025-01-17 19:51:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript for handling "Add to Cart" functionality, user interactions, etc.
+const addToCartButton = document.querySelector('button');
+
+addToCartButton.addEventListener('click', () => {
+    // Add product to cart logic here (e.g., update cart quantity, display a success message)
+    alert('Product added to cart!');
+});
